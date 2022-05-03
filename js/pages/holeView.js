8a27dbe36b507da93c59bf17fc073f1e0531deae
hoogbruin <5601109+hoogbruin@users.noreply.github.com>
2022-05-03 23:02:39 +0200

    strokesModal

--- js/pages/holeView.js
+++ js/pages/holeView.js
@@ -137,8 +137,6 @@ function holeView(hole_number) {
     // Footer
     var footer = document.createElement('div')
     footer.className = 'footer'
-    footer.id = 'hole-footer'
-    // footer_section.id = 'footer_section'
 
     var number_of_holes = Object.keys(course.holes).length
 
